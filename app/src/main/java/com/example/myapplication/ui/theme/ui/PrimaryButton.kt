21927be4b8e85e7d189e3aa54a32db3ee9a723b6
Foodics manager <foodicsmanager@Foodicss-MacBook-Pro.local>
2025-01-28 10:44:17 +0200

    add preview primary button

--- app/src/main/java/com/example/myapplication/ui/theme/ui/PrimaryButton.kt
+++ app/src/main/java/com/example/myapplication/ui/theme/ui/PrimaryButton.kt
@@ -4,11 +4,23 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 
 
 @Composable
 fun PrimaryButton(modifier: Modifier = Modifier) {
+    Button(
+        onClick = {},
+    ) {
+        Text("MyButton")
+    }
+}
 
 
-    Button(onClick = {}) { Text("MyButton") }
+@Preview
+@Composable
+private fun PreviewPrimaryButton() {
+
+
+    PrimaryButton()
 }
