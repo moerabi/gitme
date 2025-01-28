c743726fbc9ee6003932b9b753d40d8ec8c3c3a1
Foodics manager <foodicsmanager@Foodicss-MacBook-Pro.local>
2025-01-28 10:28:32 +0200

    add primary button

---
+++ app/src/main/java/com/example/myapplication/ui/theme/ui/PrimaryButton.kt
@@ -0,0 +1,14 @@
+package com.example.myapplication.ui.theme.ui
+
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+
+@Composable
+fun PrimaryButton(modifier: Modifier = Modifier) {
+
+
+    Button(onClick = {}) { Text("MyButton") }
+}
